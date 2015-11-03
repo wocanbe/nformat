6f0fa0dfc6a337442ad4634a2016356a17731320
liuying <liuying@collegepre.com>
2015-11-03 11:33:34 +0800

    Signed-off-by: liuying <wocanbe@163.com>

---
+++ config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.debug = true;
+exports.port = 3000;
+exports.email = 'wocanbe@163.com';
+exports.site_name = 'Node TODO';
+exports.site_desc = 'Very simple todo, demo for connect web dev.';
+exports.session_secret = 'todo session secret';
+
+exports.db = 'mongodb://127.0.0.1:27017/nana';
